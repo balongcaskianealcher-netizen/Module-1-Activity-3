2cdd73dab0edc0e6d5c92b88c633995f62488175
balongcaskianealcher-netizen <balongcas.kianealcher@llcc.edu.ph>
2025-12-09 10:55:47 +0800

    Add files via upload

---
+++ Activitycoding3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+cout << "Hello world" << endl;
+cout << "Hello fiona my love, Goodbye" << endl;
+cout << "Hello fiona, stay cute as always" << endl;
+return 0;
+
+}
